7389b6941bbe2b15f63fd045063e8d65c32b88af
tiagofigueiredo <tiagofigueiredo@192.168.1.65>
2020-03-14 15:53:33 +0000

    Classe Porto

--- src/trabalhogit/Porto.java
+++ src/trabalhogit/Porto.java
@@ -5,6 +5,8 @@
  */
 package trabalhogit;
 
+import java.util.*;
+
 
 
 /**
@@ -13,4 +15,36 @@ package trabalhogit;
  */
 public class Porto {
        
+    private List<Navio> navios;
+    
+    
+    //Construtor
+    public Porto(){
+        navios = new ArrayList<>();
+    }
+    
+    
+    //Adiciona Navio
+    public void addNavio(Navio n){
+        
+        if(!procura(n))
+            navios.add(n);
+        else
+            System.out.println("Matricula " + n.getMatricula() + "Já existe!");
+    }
+    
+    
+    //Procura navio
+    private boolean procura(Navio nv) {
+        for(Navio n:navios){
+            if(n.getMatricula().equals(nv.getMatricula())) return true;
+        }
+        return false;
+    }
+    
+    
+    
+    
+    
+    
 }
